61c2b3eb9f695f67f196bc068a49eacbc0bc951a
Vincent Mpenyana <61542819+vtmpenyana@users.noreply.github.com>
2020-04-08 08:04:44 +0200

    ES6 + code clean up

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var color1 = document.getElementById("color1");
-var color2 = document.getElementById("color2");
-var color3 = document.getElementById("color3");
-var output = document.getElementById("output");
-var button = document.getElementsByTagName("button")[0];
-var h2 = document.querySelector(".style");
+const color1 = document.getElementById("color1");
+const color2 = document.getElementById("color2");
+const color3 = document.getElementById("color3");
+let output = document.getElementById("output");
+const button = document.getElementsByTagName("button")[0];
+let h2 = document.querySelector(".style");
 
 
-var isMiddleOff = false;
+let isMiddleOff = false;
 
 button.addEventListener("click", function(){
 	if(isMiddleOff === false){
@@ -21,16 +21,21 @@ button.addEventListener("click", function(){
 })
 
 
-function changeColorValue(event){
-	if(color2.value === "#ffffff"){
-		output.style.background = "linear-gradient(to right, " + color1.value + ", " + color3.value +")";	
-		h2.textContent = "linear-gradient(to right, " + color1.value + ", " + color3.value +")";	
-	}
-	else{
-		output.style.background = "linear-gradient(to right," + color1.value + " , " + color2.value + ", " + color3.value +")";
-		h2.textContent = "linear-gradient(to right, " + color1.value + ", " + color2.value + ", " + color3.value +")";
-		isMiddleOff = false;
-	}
+const changeColorValue = (event) => {
+	output.style.background = (color2.value === "#ffffff") ? "linear-gradient(to right, " + color1.value + ", " + color3.value +")" :
+	"linear-gradient(to right," + color1.value + " , " + color2.value + ", " + color3.value +")";
+	h2.textContent = (color2.value === "#ffffff") ? "linear-gradient(to right, " + color1.value + ", " + color3.value +")" : 
+	"linear-gradient(to right, " + color1.value + ", " + color2.value + ", " + color3.value +")";
+
+	// if(color2.value === "#ffffff"){
+	// 	output.style.background = "linear-gradient(to right, " + color1.value + ", " + color3.value +")";	
+	// 	h2.textContent = "linear-gradient(to right, " + color1.value + ", " + color3.value +")";	
+	// }
+	// else{
+	// 	output.style.background = "linear-gradient(to right," + color1.value + " , " + color2.value + ", " + color3.value +")";
+	// 	h2.textContent = "linear-gradient(to right, " + color1.value + ", " + color2.value + ", " + color3.value +")";
+	// 	isMiddleOff = false;
+	// }
 }
 
 color1.addEventListener("input", changeColorValue)
